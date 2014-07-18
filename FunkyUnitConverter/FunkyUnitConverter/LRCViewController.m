5008b666e3b9cc334250178ca7dcdb6b7cd339f4
looneylu <lcarbuccia@gmail.com>
2014-07-18 01:34:06 -0400

    completed example

--- FunkyUnitConverter/FunkyUnitConverter/LRCViewController.m
+++ FunkyUnitConverter/FunkyUnitConverter/LRCViewController.m
@@ -9,21 +9,54 @@
 #import "LRCViewController.h"
 
 @interface LRCViewController ()
+@property (strong, nonatomic) IBOutlet UILabel *numberOfBillsLabel;
+@property (strong, nonatomic) IBOutlet UITextField *numberOfBillsTextField;
 
 @end
 
 @implementation LRCViewController
 
+
+- (IBAction)convertUnits:(id)sender
+{
+    float numberOfBills = [self.numberOfBillsTextField.text floatValue];
+    float numberOfFootballFields = numberOfBills / 91440;
+    
+    self.numberOfBillsLabel.text = [NSString stringWithFormat:@"%f", numberOfFootballFields];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    
+    int x = 5;
+    int y = 20;
+    int z = -2;
+    
+    float heightOfEverstBaseCampInFeet = 16900.3;
+    
+    // Operators include + - * /
+    
+    int additionAnswer = x + y;
+    int multiplicationAnswer = y * z;
+    NSLog(@"addition answer is %d", additionAnswer);
+    NSLog(@"multiplication answer is %d", multiplicationAnswer);
+    
+    float heightOfEverstInFeet = 29029;
+    
+    float distanceToTravel = heightOfEverstInFeet - heightOfEverstBaseCampInFeet;
+    distanceToTravel = distanceToTravel - 1000;
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    
+    NSLog(@"didReceiveMemoryWarning is evaluating");
 }
 
 @end
